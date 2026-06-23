b91702e97137018b8df3981a7f0030fc19617c06
tipsypastels <dakotasankey@gmail.com>
2026-06-22 22:05:35 -0700

    cries

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
 import "./main.css";
+
+for (const element of document.querySelectorAll("[data-audio]")) {
+  const path = (element as HTMLElement).dataset.audio!;
+  const audio = new Audio(path);
+
+  let even = true;
+
+  element.addEventListener("click", async () => {
+    const parityClass = `playing--${even ? "even" : "odd"}`;
+
+    element.classList.add("playing", parityClass);
+
+    await new Promise((resolve) => {
+      audio.onended = resolve;
+      audio.play();
+    });
+
+    element.classList.remove("playing", parityClass);
+    even = !even;
+  });
+}
